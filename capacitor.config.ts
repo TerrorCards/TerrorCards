db5545b5beceb6a1d99b41f00cd8d52b2560dc78
TerrorCards <51006599+TerrorCards@users.noreply.github.com>
2023-01-25 06:54:21 -0800

    sync 3

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.terrorcards.app',
+  appName: 'TerrorCards',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
