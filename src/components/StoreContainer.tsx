3bf72c0b1803c752fb780d925af75c8a8c7f32f2
TerrorCards <support@TerrorCards.com>
2026-08-04 22:04:44 -0700

    revert IAP

--- src/components/StoreContainer.tsx
+++ src/components/StoreContainer.tsx
@@ -35,7 +35,6 @@ interface props {
 interface state {
   allItemsList: Array<any>;
   allCoinList: Array<any>;
-  allCoinMetaList: Array<any>;
   packItems: Array<any>;
   storeType: string;
   showCards: boolean;
@@ -46,36 +45,22 @@ interface state {
   showConfirmPurchase: boolean;
   targetItem: any;
   targetType: any;
-  purchaseErrorMsg: string;
   showCoinMessage: boolean;
   coinPurchaseMsg: any;
   isInAppLoaded: boolean;
-  isInAppInitializing: boolean;
   isIAPActiveBuy: boolean;
-  isPackActiveBuy: boolean;
 }
 
-const purchaseStore = window.CdvPurchase;
-const store = purchaseStore?.store;
-const ProductType = purchaseStore?.ProductType;
-const Platform = purchaseStore?.Platform;
+const { store, ProductType, Platform } = window.CdvPurchase;
 let inAppControl = 0;
-let isStoreRegistered = false;
-let isStoreInitialized = false;
-let areStoreHandlersBound = false;
-let pendingVerificationKey: string | null = null;
-let lastProcessedVerificationKey: string | null = null;
 
 class StoreContainer extends React.Component<props, state> {
-  packBuyLock = false;
-
   constructor(props: any) {
     super(props);
 
     this.state = {
       allItemsList: [],
       allCoinList: [],
-      allCoinMetaList: [],
       packItems: [],
       storeType: "regular",
       showCards: false,
@@ -86,13 +71,10 @@ class StoreContainer extends React.Component<props, state> {
       showConfirmPurchase: false,
       targetItem: null,
       targetType: null,
-      purchaseErrorMsg: "",
       showCoinMessage: false,
       coinPurchaseMsg: null,
       isInAppLoaded: false,
-      isInAppInitializing: false,
       isIAPActiveBuy: false,
-      isPackActiveBuy: false,
     };
   }
 
@@ -114,7 +96,7 @@ class StoreContainer extends React.Component<props, state> {
     this.pullPacks();
     Device.getInfo().then((d: any) => {
       this.deviceInfo.platform = d.platform;
-      if (!d.isVirtual && store && ProductType && Platform) {
+      if (!d.isVirtual) {
         this.pullInApp();
       }
     });
@@ -122,9 +104,9 @@ class StoreContainer extends React.Component<props, state> {
 
   ionViewWillEnter() {
     this.pullPacks();
-    if (!this.state.isInAppLoaded && !this.state.isInAppInitializing) {
-      this.pullInApp();
-    }
+    //if (!this.state.isInAppLoaded) {
+    //this.pullInApp();
+    //}
   }
 
   componentWillMount() {
@@ -136,207 +118,6 @@ class StoreContainer extends React.Component<props, state> {
   ionViewDidEnter() {}
 
   ionViewDidLeave() {}
-
-  getPurchaseValue = (productId: string | null) => {
-    if (!productId) {
-      return 0;
-    }
-
-    const matchedProduct = this.state.allCoinMetaList.find((item: any) => {
-      return item.ID === productId;
-    });
-    const mappedValue = parseInt(
-      matchedProduct?.Credit ||
-        matchedProduct?.credit ||
-        matchedProduct?.Value ||
-        matchedProduct?.value ||
-        matchedProduct?.Amount ||
-        matchedProduct?.amount ||
-        matchedProduct?.Coins ||
-        matchedProduct?.coins ||
-        0,
-      10
-    );
-
-    if (!Number.isNaN(mappedValue) && mappedValue > 0) {
-      return mappedValue;
-    }
-
-    if (productId.indexOf("25k") > -1) {
-      return 25000;
-    }
-    if (productId.indexOf("100k") > -1) {
-      return 100000;
-    }
-    if (productId.indexOf("250k") > -1) {
-      return 250000;
-    }
-    if (productId.indexOf("500k") > -1) {
-      return 500000;
-    }
-    if (productId.indexOf("750k") > -1) {
-      return 750000;
-    }
-    if (productId.indexOf("1m") > -1) {
-      return 1000000;
-    }
-
-    return 0;
-  };
-
-  updateCreditBalance = async (value: number) => {
-    const response = await callServer(
-      "updateCredit",
-      { credit: value },
-      this.props.user.ID
-    );
-
-    if (!response || !response.ok) {
-      throw new Error("Credit update request failed.");
-    }
-
-    let payload: any = null;
-
-    try {
-      payload = await response.json();
-    } catch (err) {
-      payload = null;
-    }
-
-    if (
-      payload === false ||
-      payload === null ||
-      payload === "" ||
-      payload === "Error" ||
-      payload?.success === false ||
-      payload?.status === "error"
-    ) {
-      throw new Error("Credit update was not confirmed by the server.");
-    }
-
-    return payload;
-  };
-
-  getVerifiedProductId = (purchase: any) => {
-    const transactions = purchase?.sourceReceipt?.transactions || [];
-
-    if (this.deviceInfo.platform === "android") {
-      return transactions?.[0]?.products?.[0]?.id || null;
-    }
-
-    for (const transaction of transactions) {
-      const transactionProductId = transaction?.products?.[0]?.id;
-      if (
-        transactionProductId &&
-        (!this.state.targetItem || transactionProductId === this.state.targetItem.id)
-      ) {
-        return transactionProductId;
-      }
-    }
-
-    return transactions?.[0]?.products?.[0]?.id || null;
-  };
-
-  getVerificationKey = (purchase: any) => {
-    const transaction = purchase?.sourceReceipt?.transactions?.[0];
-
-    return (
-      transaction?.transactionId ||
-      transaction?.purchaseToken ||
-      transaction?.id ||
-      purchase?.transactionId ||
-      purchase?.id ||
-      null
-    );
-  };
-
-  bindStoreHandlers = () => {
-    if (!store || areStoreHandlersBound) {
-      return;
-    }
-
-    store
-      .when()
-      .approved((purchase: any) => purchase.verify())
-      .verified((purchase: any) => {
-        const verificationKey = this.getVerificationKey(purchase);
-        const productId = this.getVerifiedProductId(purchase);
-        const value = this.getPurchaseValue(productId);
-        let shouldFinishPurchase = false;
-
-        if (
-          verificationKey &&
-          (verificationKey === pendingVerificationKey ||
-            verificationKey === lastProcessedVerificationKey)
-        ) {
-          shouldFinishPurchase = true;
-          purchase.finish();
-          return;
-        }
-
-        if (inAppControl !== 1 || value === 0) {
-          inAppControl = 0;
-          this.setState({
-            isIAPActiveBuy: false,
-            purchaseErrorMsg:
-              value === 0
-                ? "Purchase completed, but the credit amount could not be matched to this product."
-                : "Purchase verification was not tied to an active coin order.",
-          });
-          shouldFinishPurchase = true;
-          purchase.finish();
-          return;
-        }
-
-        inAppControl = 0;
-        pendingVerificationKey = verificationKey;
-
-        this.updateCreditBalance(value)
-          .then(() => {
-            lastProcessedVerificationKey = verificationKey;
-            pendingVerificationKey = null;
-            shouldFinishPurchase = true;
-            this.setState({
-              targetItem: null,
-              targetType: null,
-              purchaseErrorMsg: "",
-              showCoinMessage: true,
-              coinPurchaseMsg:
-                "Thank you. Account updated by " + value + " credit",
-              isIAPActiveBuy: false,
-            });
-            this.props.callbackPackOpenTimer(Date.now());
-          })
-          .catch((err: any) => {
-            pendingVerificationKey = null;
-            inAppControl = 1;
-            this.setState({
-              isIAPActiveBuy: false,
-              purchaseErrorMsg:
-                "Your purchase was verified, but the app could not confirm the credit update. Please contact support before retrying.",
-            });
-            console.log(err);
-          })
-          .finally(() => {
-            if (shouldFinishPurchase) {
-              purchase.finish();
-            }
-          });
-      })
-      .unverified((purchase: any) => {
-        inAppControl = 0;
-        this.setState({
-          isIAPActiveBuy: false,
-          purchaseErrorMsg:
-            "The purchase could not be verified. You were not credited.",
-        });
-        if (purchase?.finish) {
-          purchase.finish();
-        }
-      });
-
-    areStoreHandlersBound = true;
-  };
 
   pullPacks = () => {
     callServer("packs", "", this.props.user.ID)
@@ -357,18 +138,13 @@ class StoreContainer extends React.Component<props, state> {
   };
 
   pullInApp = () => {
-    if (!store || !ProductType || !Platform || this.state.isInAppInitializing) {
-      return;
-    }
-
-    this.setState({ isInAppInitializing: true });
-
     callServer("loadInAppItems", "", this.props.user.ID)
       ?.then((resp) => {
         return resp.json();
       })
       .then((json) => {
         if (json.length > 0) {
+          //const store: any = new window.CdvPurchase.Store();
           const whatPlatform =
             this.deviceInfo.platform === "android"
               ? Platform.GOOGLE_PLAY
@@ -382,40 +158,75 @@ class StoreContainer extends React.Component<props, state> {
               type: ProductType.CONSUMABLE,
             });
           });
-          if (!isStoreRegistered) {
-            store.register(productList);
-            isStoreRegistered = true;
-          }
+          //alert(productList.toString());
+          store.register(productList);
+          store
+            .when()
+            .approved((p: any) => p.verify())
+            .verified((p: any) => {
+              let productId = null;
+              if (this.deviceInfo.platform === "android") {
+                productId = p.sourceReceipt.transactions[0].products[0].id;
+              } else {
+                const trans = p.sourceReceipt.transactions;
+                trans.forEach((tran: any) => {
+                  if (tran.products[0].id === this.state.targetItem.id) {
+                    productId = tran.products[0].id;
+                  }
+                });
+              }
+              //alert(JSON.stringify(p.sourceReceipt.transactions));
+              let value = 0;
+              if (productId.indexOf("25k") > -1) {
+                value = 25000;
+              } else if (productId.indexOf("100k") > -1) {
+                value = 100000;
+              } else if (productId.indexOf("250k") > -1) {
+                value = 250000;
+              } else if (productId.indexOf("500k") > -1) {
+                value = 500000;
+              } else if (productId.indexOf("750k") > -1) {
+                value = 750000;
+              } else if (productId.indexOf("1m") > -1) {
+                value = 1000000;
+              } else {
+                value = 0;
+              }
+              //alert(value);
+              if (inAppControl === 1) {
+                callServer(
+                  "updateCredit",
+                  { credit: value },
+                  this.props.user.ID
+                )?.then((result: any) => {
+                  this.setState({
+                    targetItem: null,
+                    targetType: null,
+                    showCoinMessage: true,
+                    coinPurchaseMsg:
+                      "Thank you. Account updated by " + value + " credit",
+                    isIAPActiveBuy: false,
+                  });
+                  this.props.callbackPackOpenTimer(Date.now());
+                });
+                inAppControl = 0;
+              }
+              p.finish();
+            });
 
-          this.bindStoreHandlers();
-
-          const initializeStore = isStoreInitialized
-            ? Promise.resolve()
-            : store.initialize([whatPlatform]).then(() => {
-                isStoreInitialized = true;
-              });
-
-          initializeStore.then(() => {
+          store.initialize([whatPlatform]).then(() => {
+            //alert(this.deviceInfo.platform);
             store.ready(() => {
+              //alert(store.products);
               this.setState({
-                allCoinMetaList: items,
-                allCoinList: [...store.products],
+                allCoinList: store.products,
                 isInAppLoaded: true,
-                isInAppInitializing: false,
               });
             });
-          });
-        } else {
-          this.setState({
-            allCoinMetaList: [],
-            allCoinList: [],
-            isInAppLoaded: false,
-            isInAppInitializing: false,
           });
         }
       })
       .catch((err: any) => {
-        this.setState({ isInAppInitializing: false });
         console.log(err);
       });
   };
@@ -479,8 +290,9 @@ class StoreContainer extends React.Component<props, state> {
                             //this._canBuy(p);
                           }}
                           disabled={
-                            this.state.isPackActiveBuy ||
-                            parseInt(this.props.user.credit) < parseInt(p.Cost)
+                            parseInt(this.props.user.credit) >= parseInt(p.Cost)
+                              ? false
+                              : true
                           }
                         >
                           {p.Cost}
@@ -515,26 +327,19 @@ class StoreContainer extends React.Component<props, state> {
 
   renderCoinsList = () => {
     let items: Array<any> = [];
-    if (this.state.isInAppInitializing) {
-      items.push(
-        <IonItem key={"coin-loading"}>
-          <IonLabel>Loading purchase options</IonLabel>
-          <IonSpinner></IonSpinner>
-        </IonItem>
-      );
-    }
     if (this.state.allCoinList.length > 0) {
       if (this.state.isIAPActiveBuy) {
         items.push(
-          <IonItem key={"coin-processing"}>
+          <IonItem>
             <IonLabel>Processing, please wait </IonLabel>
             <IonSpinner></IonSpinner>
           </IonItem>
         );
       }
       this.state.allCoinList.forEach((p: any) => {
-        const pricing = p.offers?.[0]?.pricingPhases?.[0];
-        if (p.title !== "" && pricing) {
+        //alert(JSON.stringify(p));
+        if (p.title !== "") {
+          const pricing = p.offers[0].pricingPhases[0];
           items.push(
             <IonCard key={p.title}>
               <IonCardHeader>
@@ -553,15 +358,12 @@ class StoreContainer extends React.Component<props, state> {
                         >
                           <IonButton
                             expand="block"
-                            disabled={
-                              this.state.isIAPActiveBuy ||
-                              this.state.isInAppInitializing
-                            }
                             onClick={() => {
                               this.setState({
                                 showConfirmPurchase: true,
                                 targetItem: p,
                                 targetType: "coin",
+                                isIAPActiveBuy: true,
                               });
 
                               //alert(JSON.stringify(p));
@@ -581,16 +383,6 @@ class StoreContainer extends React.Component<props, state> {
         }
       });
     }
-    if (!this.state.isInAppInitializing && this.state.allCoinList.length === 0) {
-      items.push(
-        <IonCard key={"noCoinOptions"}>
-          <IonCardContent>
-            Purchase options are not available yet. Re-open the Coins tab to
-            retry.
-          </IonCardContent>
-        </IonCard>
-      );
-    }
     items.push(
       <IonCard key={"spaceerCoin"}>
         <IonCardContent>
@@ -604,15 +396,14 @@ class StoreContainer extends React.Component<props, state> {
         </IonCardContent>
       </IonCard>
     );
-
-    return items;
+    this.setState({ packItems: items });
   };
 
   changeStoreType = (value: string) => {
     this.setState({ storeType: value }, () => {
       if (value === "coins") {
-        if (!this.state.isInAppLoaded && !this.state.isInAppInitializing) {
-          this.pullInApp();
+        if (this.state.isInAppLoaded) {
+          this.renderCoinsList();
         }
       } else {
         this.filterPacks();
@@ -622,18 +413,10 @@ class StoreContainer extends React.Component<props, state> {
 
   //Buying checks
   _canBuy = () => {
-    if (this.packBuyLock || this.state.isPackActiveBuy) {
-      return;
-    }
-
     //call server to get latest credit and see if user can buy
     if (this.state.targetItem !== null) {
       const p = this.state.targetItem;
       if (parseInt(this.props.user.credit) >= parseInt(p.Cost)) {
-        // Lock synchronously so rapid taps cannot enqueue duplicate requests
-        this.packBuyLock = true;
-        this.setState({ isPackActiveBuy: true });
-
         //call to pull packs.
         let packOrder = {
           packID: p.ID,
@@ -658,10 +441,6 @@ class StoreContainer extends React.Component<props, state> {
           })
           .catch((err: any) => {
             console.log(err);
-          })
-          .finally(() => {
-            this.packBuyLock = false;
-            this.setState({ isPackActiveBuy: false });
           });
       } else {
         this.setState({ showNoCoinAlert: true });
@@ -721,11 +500,7 @@ class StoreContainer extends React.Component<props, state> {
           </IonSegmentButton>
         </IonSegment>
 
-        <IonList>
-          {this.state.storeType === "coins"
-            ? this.renderCoinsList()
-            : this.state.packItems}
-        </IonList>
+        <IonList>{this.state.packItems}</IonList>
 
         <IonModal
           isOpen={this.state.showCards}
@@ -791,25 +566,6 @@ class StoreContainer extends React.Component<props, state> {
               cssClass: "secondary",
               handler: (blah: any) => {
                 this.setState({ showConfirmPurchase: false });
-              },
-            },
-          ]}
-        />
-
-        <IonAlert
-          isOpen={this.state.purchaseErrorMsg !== ""}
-          onDidDismiss={() => {
-            this.setState({ purchaseErrorMsg: "" });
-          }}
-          header="Purchase Issue"
-          message={this.state.purchaseErrorMsg}
-          buttons={[
-            {
-              text: "Ok",
-              role: "cancel",
-              cssClass: "secondary",
-              handler: () => {
-                this.setState({ purchaseErrorMsg: "" });
               },
             },
           ]}
@@ -923,26 +679,16 @@ class StoreContainer extends React.Component<props, state> {
     alert(JSON.stringify(offer));
     if (offer) offer.order();
     */
-    if (
-      this.state.isIAPActiveBuy ||
-      this.state.isInAppInitializing ||
-      this.state.targetItem === null
-    ) {
-      return;
-    }
-
     const foundProduct = this.state.allCoinList.filter((coins) => {
       return coins.id === this.state.targetItem.id;
     });
-
+    //alert(JSON.stringify(foundProduct));
     if (foundProduct.length > 0) {
-      const offer = foundProduct[0].getOffer?.();
-      if (offer) {
-        inAppControl = 1;
-        this.setState({ isIAPActiveBuy: true }, () => {
-          offer.order();
-        });
-      }
+      const offer = foundProduct[0].getOffer();
+      //alert("offer");
+      //alert(JSON.stringify(offer));
+      inAppControl = 1;
+      if (offer) offer.order();
     }
   };
 }
